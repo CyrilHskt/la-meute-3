fe5500b919de3093a61ed93917cd0900df425001
Cyril Hskt <cyrilhskt@proton.me>
2026-07-31 11:20:14 +0200

    Enlarge value icons another ~30% (80px -> 105px)

--- front/src/components/gouvernance/GovernancePresentation.vue
+++ front/src/components/gouvernance/GovernancePresentation.vue
@@ -128,7 +128,7 @@ const values = computed(() => [
         <ul class="gv-values-grid">
           <li v-for="value in values" :key="value.title" class="gv-value-item">
             <span class="gv-value-index mono">{{ value.index }}</span>
-            <img :src="value.icon" :alt="value.iconAlt" class="gv-value-icon" width="80" height="80" loading="lazy" />
+            <img :src="value.icon" :alt="value.iconAlt" class="gv-value-icon" width="105" height="105" loading="lazy" />
             <div>
               <h4>{{ value.title }}</h4>
               <p>{{ value.text }}</p>
@@ -475,8 +475,8 @@ const values = computed(() => [
 .gv-value-icon {
   flex: none;
   align-self: center;
-  width: 80px;
-  height: 80px;
+  width: 105px;
+  height: 105px;
   object-fit: contain;
   opacity: 0.92;
   filter: sepia(35%) saturate(70%) contrast(95%);
@@ -488,8 +488,8 @@ const values = computed(() => [
 
 @media (max-width: 480px) {
   .gv-value-icon {
-    width: 56px;
-    height: 56px;
+    width: 72px;
+    height: 72px;
   }
 }
 
